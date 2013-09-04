bdbb8b8ce1950ad4323618405f57a4e6e460d275
Wubai Zhou <zhouwubai@gmail.com>
2013-09-03 22:33:27 -0400

    add tabbar controller class.

---
+++ Uslunchbox/tabbarVC/MainLunchTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainLunchTabBarController.h
+//  Uslunchbox
+//
+//  Created by Wubai Zhou on 9/3/13.
+//  Copyright (c) 2013 wubai zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainLunchTabBarController : UITabBarController <UINavigationControllerDelegate>
+
+@end
---
+++ Uslunchbox/tabbarVC/MainLunchTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  MainLunchTabBarController.m
+//  Uslunchbox
+//
+//  Created by Wubai Zhou on 9/3/13.
+//  Copyright (c) 2013 wubai zhou. All rights reserved.
+//
+
+#import "MainLunchTabBarController.h"
+
+@interface MainLunchTabBarController ()
+
+@end
+
+@implementation MainLunchTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
